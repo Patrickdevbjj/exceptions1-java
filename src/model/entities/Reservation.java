435b63e696ff9c55542f8feb7451a643e8279781
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-12-15 15:53:06 -0300

    Adicionando a logica da Reservation

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model.entities;
 
-/**
- *
- * @author User
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     
-}
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return    TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+    
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+    
+    @Override
+    public String toString() {
+        return "room "
+               + roomNumber
+               + ", check-in: "
+               +  sdf.format(checkOut)
+               + ", "
+               + duration() 
+               + " nights"
+    }
+}   
