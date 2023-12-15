dbcb3b0c20dd8f7d3ce899011b9bba517792a9f9
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-12-15 16:25:26 -0300

    Adicionando a logica da main

--- src/exceptions1/Exceptions1.java
+++ src/exceptions1/Exceptions1.java
@@ -1,10 +1,35 @@
 
 package exceptions1;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import model.entities.Reservation;
+
 public class Exceptions1 {
 
    
-    public static void main(String[] args) {
-            }
-    
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        
+        
+        System.out.println("Room Number: ");
+        int number = sc.nextInt();
+        System.out.println("Check-in Date (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.println("Check-out Date (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+        
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation dates!!");
+        }
+        else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+        }
+        
+        sc.close();
+    }
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -53,6 +53,6 @@ public class Reservation {
                +  sdf.format(checkOut)
                + ", "
                + duration() 
-               + " nights"
+               + " nights";
     }
 }   
